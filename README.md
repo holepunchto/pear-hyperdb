b7ea70d928b692b2abe7dbbde95756d736d025cb
Andrei Regiani <andrei.cpp@gmail.com>
2026-02-08 21:20:56 -0600

    🍐💾

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# pear-db
-HyperDB spec and model for Pear
+# pear-hyperdb
+
+[HyperDB](https://github.com/holepunchto/hyperdb) spec and model for [Pear](https://github.com/holepunchto/pear)
+
+```
+npm install pear-hyperdb
+```
+
+## Usage
+
+### Model
+
+```js
+const Model = require('pear-hyperdb')
+```
+
+```js
+const model = new Model(corestore)
+const nodes = await model.getDhtNodes()
+console.log(nodes)
+```
+
+### Spec
+
+```js
+const dbSpec = require('pear-hyperdb/spec/db')
+```
+
+## License
+
+Apache-2.0
---
+++ build.js
@@ -0,0 +1,289 @@
+/* SCHEMA SAFETY RULES
+
+1. Schema fields are append-only: once a new field is added, it cannot be removed (comment as deprecated instead)
+2. Deleting/resetting files in `./spec` directory is forbidden
+3. After making changes: `npm run build`. Once merged into the main branch, there is no undoing.
+*/
+const path = require('bare-path')
+const Hyperschema = require('hyperschema')
+const Builder = require('hyperdb/builder')
+
+const SCHEMA_DIR = path.join(__dirname, 'spec', 'schema')
+const DB_DIR = path.join(__dirname, 'spec', 'db')
+
+// hyperdb/schema
+const schema = Hyperschema.from(SCHEMA_DIR, { versioned: false })
+const pearSchema = schema.namespace('pear')
+
+// custom types
+pearSchema.register({
+  name: 'node',
+  fields: [
+    {
+      name: 'host',
+      type: 'string',
+      required: true
+    },
+    {
+      name: 'port',
+      type: 'uint',
+      required: true
+    }
+  ]
+})
+
+pearSchema.register({
+  name: 'presets',
+  fields: [
+    {
+      name: 'link',
+      type: 'string',
+      required: true
+    },
+    {
+      name: 'command',
+      type: 'string',
+      required: true
+    },
+    {
+      name: 'flags',
+      type: 'string',
+      required: true
+    }
+  ]
+})
+
+pearSchema.register({
+  name: 'checkout',
+  fields: [
+    {
+      name: 'fork',
+      type: 'uint',
+      required: true
+    },
+    {
+      name: 'length',
+      type: 'uint',
+      required: true
+    }
+  ]
+})
+
+pearSchema.register({
+  // DEPRECATED
+  name: 'pack',
+  fields: [
+    {
+      name: 'bundle',
+      type: 'string',
+      required: true
+    },
+    {
+      name: 'entry',
+      type: 'string',
+      required: true
+    },
+    {
+      name: 'builtins',
+      type: 'string',
+      array: true,
+      required: false
+    },
+    {
+      name: 'conditions',
+      type: 'string',
+      array: true,
+      required: false
+    },
+    {
+      name: 'extensions',
+      type: 'string',
+      array: true,
+      required: false
+    }
+  ]
+})
+
+// both structs & custom types
+pearSchema.register({
+  name: 'assets',
+  fields: [
+    {
+      name: 'link',
+      type: 'string',
+      required: true
+    },
+    {
+      name: 'ns',
+      type: 'string',
+      required: true
+    },
+    {
+      name: 'path',
+      type: 'string',
+      required: true
+    },
+    {
+      name: 'name',
+      type: 'string'
+    },
+    {
+      name: 'only',
+      type: 'string',
+      array: true
+    },
+    {
+      name: 'bytes',
+      type: 'uint'
+    },
+    {
+      // DEPRECATED
+      name: 'pack',
+      type: '@pear/pack',
+      array: true
+    }
+  ]
+})
+
+// structs
+pearSchema.register({
+  name: 'manifest',
+  fields: [
+    {
+      name: 'version',
+      type: 'uint',
+      required: true
+    }
+  ]
+})
+
+pearSchema.register({
+  name: 'dht',
+  fields: [
+    {
+      name: 'nodes',
+      type: '@pear/node',
+      array: true
+    }
+  ]
+})
+
+pearSchema.register({
+  name: 'traits',
+  fields: [
+    {
+      name: 'link',
+      type: 'string',
+      required: true
+    },
+    {
+      name: 'appStorage',
+      type: 'string',
+      required: true
+    },
+    {
+      name: 'encryptionKey',
+      type: 'fixed32'
+    },
+    {
+      name: 'tags',
+      type: 'string',
+      array: true
+    }
+  ]
+})
+
+pearSchema.register({
+  name: 'gc',
+  fields: [
+    {
+      name: 'path',
+      type: 'string',
+      required: true
+    }
+  ]
+})
+
+pearSchema.register({
+  name: 'current',
+  fields: [
+    {
+      name: 'link',
+      type: 'string',
+      required: true
+    },
+    {
+      name: 'checkout',
+      type: '@pear/checkout'
+    },
+    {
+      name: 'key',
+      type: 'fixed32'
+    }
+  ]
+})
+
+Hyperschema.toDisk(schema)
+
+// hyperdb/db
+const db = Builder.from(SCHEMA_DIR, DB_DIR)
+const pearDB = db.namespace('pear')
+pearDB.require(path.join(__dirname, 'spec', 'helpers.js'))
+
+pearDB.collections.register({
+  name: 'manifest',
+  schema: '@pear/manifest'
+})
+
+pearDB.collections.register({
+  name: 'dht',
+  schema: '@pear/dht'
+})
+
+pearDB.collections.register({
+  name: 'gc',
+  schema: '@pear/gc',
+  key: ['path']
+})
+
+pearDB.collections.register({
+  name: 'traits',
+  schema: '@pear/traits',
+  key: ['link']
+})
+
+pearDB.collections.register({
+  name: 'assets',
+  schema: '@pear/assets',
+  key: ['link']
+})
+
+pearDB.collections.register({
+  name: 'current',
+  schema: '@pear/current',
+  key: ['link']
+})
+
+pearDB.collections.register({
+  name: 'presets',
+  schema: '@pear/presets',
+  key: ['link']
+})
+
+pearDB.indexes.register({
+  name: 'traits-by-tags',
+  collection: '@pear/traits',
+  unique: false,
+  key: {
+    type: 'string',
+    map: 'tags'
+  }
+})
+
+pearDB.indexes.register({
+  name: 'presets-by-command',
+  collection: '@pear/presets',
+  key: ['link', 'command']
+})
+
+Builder.toDisk(db)
